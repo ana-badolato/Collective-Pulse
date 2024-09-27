8753744f27e8d0001a5a03e7058a6a60995f62e0
Ana Badolato <badolatoana@gmail.com>
2024-09-27 12:56:38 +0200

    Project ready to go
    Co-authored-by: Javier Gascon Ruiz <Javitocatral@users.noreply.github.com>

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx'
+import { BrowserRouter as Router } from "react-router-dom"
+
+createRoot(document.getElementById('root')).render(
+  <Router>
+        <App />
+  </Router>
+)
