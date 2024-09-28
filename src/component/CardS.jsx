974847107a834857a6e17b36ff3809f86c65061f
Ana Badolato <badolatoana@gmail.com>
2024-09-28 11:40:23 +0200

    Fixes on navbar: - Wrapper around content in navbar - Mobile menu now hides properly

--- src/component/CardS.jsx
+++ src/component/CardS.jsx
@@ -1,11 +1,11 @@
-import naturaleza from '../assets/naturalez.png'
+import nature from '../assets/naturalez.png'
 function CardS() {
   return (
-    <div class="contenedor">
-      <div class="imagen">
-        <img src={naturaleza} alt="" />
+    <div className="container">
+      <div className="image">
+        <img src={nature} alt="" />
       </div>
-      <div class="info">
+      <div className="info">
         <h3>TITULAR</h3>
         <p>Este es un resumen del la noticia y todo es clicable</p>
       </div>
--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,25 +1,28 @@
-import { Link } from 'react-router-dom'
-import menu from '../assets/menu.png'
+import { Link } from "react-router-dom";
+import menu from "../assets/menu.png";
 
 function Navbar() {
   return (
     <div className="container-nav">
-      <div>
-        <img className="menu" src={menu} alt="menu" />
-      </div>
-      <div className="logo">
-        <Link to="/">COLLECTIVE PULSE</Link>
-      </div>
-      <div className="conatiner-categories-nav">
-        <Link to="/category">CIVIS</Link>
-        <Link to="/category">CULTURE</Link>
-        <Link to="/category">SCIENCE</Link>
-        <Link to="/category">LIFESTYLE</Link>
-        <Link to="/category">SUSTAINABILITY</Link>
-        <Link to="/category">TRAVEL</Link>
+      <div className="wrapper-nav">
+        <div className="container-menu">
+          <img className="menu" src={menu} alt="menu" />
+        </div>
+   
+        <div className="logo">
+          <Link to="/">COLLECTIVE PULSE</Link>
+        </div>
+        <div className="container-categories-nav">
+          <Link to="/category">CIVIS</Link>
+          <Link to="/category">CULTURE</Link>
+          <Link to="/category">SCIENCE</Link>
+          <Link to="/category">LIFESTYLE</Link>
+          <Link to="/category">SUSTAINABILITY</Link>
+          <Link to="/category">TRAVEL</Link>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
