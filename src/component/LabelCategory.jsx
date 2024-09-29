392952fff92aae87b805a2c1c55818ca6b5e1b35
Javier Gascon <javitocatral@gmail.com>
2024-09-29 19:33:14 +0200

    label an efects unfinishedd

---
+++ src/component/LabelCategory.jsx
@@ -0,0 +1,13 @@
+import science from '../assets/icons/scienceIcon.png'
+function LabelCategory() {
+  return (
+    <div class="box-labelcategory">
+      <div class="labelcategory">
+        <img src={science} alt="" />
+        <h4>SCIENCE</h4>
+      </div>
+    </div>
+  )
+}
+
+export default LabelCategory
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,31 +1,35 @@
 import Carousel from '../component/Carousel'
 
-import SearchBar from '../component/SearchBar';
+import SearchBar from '../component/SearchBar'
 import CardM from '../component/CardM'
+import LabelCategory from '../component/LabelCategory'
 function Home() {
   return (
     <div className="home">
       <div className="hero"></div>
       <SearchBar />
-      <h1 className='title'><span>COLLECTIVE</span> <br />PULSE</h1>
+      <h1 className="title">
+        <span>COLLECTIVE</span> <br />
+        PULSE
+      </h1>
       <Carousel />
-      <div className='section-header'>
+      <div className="section-header">
         <h2>LATEST</h2>
         <hr />
       </div>
-      <div className='cardM-section' >
+      <div className="cardM-section">
+        <LabelCategory />
         <CardM />
         <CardM />
         <CardM />
         <CardM />
       </div>
       {/* el slider iría aquí entre sección y sección */}
-      <div className='section-header'>
+      <div className="section-header">
         <h2>TRENDING</h2>
         <hr />
       </div>
     </div>
-   
   )
 }
 
