3ecb96cbcd27f3cc3e32809e65c45b1c297a062b
Ana Badolato <badolatoana@gmail.com>
2024-10-03 15:50:20 +0200

    searchresults min working

--- src/pages/SearchResults.jsx
+++ src/pages/SearchResults.jsx
@@ -1,23 +1,48 @@
 import { useState } from "react";
 import CardM from "../component/CardM";
 import SearchBar from "../component/SearchBar";
-
+import { Link } from "react-router-dom";
 
 function SearchResults(props) {
-  
-        const{searchValue,setSearchValue,handleSearchChange, getCategoryColor, filteredNews} =props
+  const { getRandomAvatar } = props;
+  const {
+    searchValue,
+    setSearchValue,
+    handleSearchChange,
+    getCategoryColor,
+    filteredNews,
+  } = props;
   return (
-    <div>
-      <div>SearchResults</div>
+    <div className="searchResults-container">
+      <div className="hero-category"></div>
+      <div className="section-header">
+        <h2>LATEST</h2>
+        <hr />
+      </div>
       <SearchBar
         searchValue={searchValue}
         setSearchValue={setSearchValue}
         handleSearchChange={handleSearchChange}
       />
-
-      {filteredNews.map((eachNew) => {
-        return <CardM getCategoryColor={getCategoryColor} key={eachNew.id} {...eachNew} />;
-      })}
+      <div className="cardM-section-results">
+        {filteredNews.map((eachNew) => {
+          return (
+            <Link
+                className="link-cardM"
+                key={eachNew.id}
+                to={`/details/${eachNew.id}`}
+                onClick={() => incrementViews(eachNew.id)}
+              >
+            <CardM
+              getCategoryColor={getCategoryColor}
+              getRandomAvatar={getRandomAvatar}
+              key={eachNew.id}
+              {...eachNew}
+            />
+            </Link>
+          );
+        })}
+      </div>
     </div>
   );
 }
